bfd272b0b6c1fa649e3ab595518efe9a4d339f87
Aaron Osteraas <aaron.osteraas@gmail.com>
2017-08-13 13:08:20 +1000

    position returning through promise based lookup

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,19 +1,28 @@
 'use strict';
 
+const geoErrMsg = "Unfortunately your browser does not support Geolocation or Geolocation failed. Please proceed to enter your location manually.";
+
 document.addEventListener('DOMContentLoaded', () => {
   let modal = document.querySelector('.location-check');
   // check if location permission has previously been granted
   if (localStorage.geoPermission === "true") {
-    modal.classList.remove('is-active');
-    // then check weather
+    // get position
+    getPosition().then((pos) => {
+      // remove modal on success
+      modal.classList.remove('is-active');
+      console.log(pos);
+    }).catch((err) => {
+      document.querySelector('.modal .location-message').innerText = geoErrMsg;
+    });
   }
   document.querySelector('.modal .button').addEventListener('click', () => {
     if (geoCheck()) {
       modal.classList.remove('is-active');
-
+      getPosition().then((pos) => {
+        console.log(pos);
+      });
     } else {
-      document.querySelector('.location-message').innerText = "Unfortunately your browser does not support geolocation." +
-          " Please proceed to enter your location manually."
+      document.querySelector('.modal .location-message').innerText = geoErrMsg;
     }
   });
   document.querySelector('.modal-close').addEventListener('click', () => {
@@ -21,6 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 });
 
+function getPosition () {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+function geoFail (err) {
+
+}
 // func to check if browser supports geolocation
 function geoCheck() {
   if ("geolocation" in navigator) {
@@ -37,19 +55,6 @@ function locationPermission() {
   }
 }
 
-if (localStorage.geoPermission === "true") {
-  if ("geolocation" in navigator) {
-    $('#popupLoading').show();
-    $('#popup').hide();
-
-    getWeatherAndReverseGeo().then(function () {
-
-      $('#popupLoading').fadeOut();
-    });
-  } else {
-    alert("Your browser does not support geolocation, please enter your location manually");
-  }
-}
 
 
 // getWeatherAndReverseGeo();
