f1e7bca531983fa9574985d4ef23ff24d87559e7
Aaron Osteraas <aaron.osteraas@gmail.com>
2017-08-13 13:31:32 +1000

    reset api keys, new keys in env file, add sample env file to project

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -4,25 +4,30 @@ const geoErrMsg = "Unfortunately your browser does not support Geolocation or Ge
 
 document.addEventListener('DOMContentLoaded', () => {
   let modal = document.querySelector('.location-check');
+  let locMsg = document.querySelector('.modal .location-message');
+  let lat = document.querySelector('.latitude');
+  let lon = document.querySelector('.longitude');
   // check if location permission has previously been granted
   if (localStorage.geoPermission === "true") {
     // get position
     getPosition().then((pos) => {
       // remove modal on success
       modal.classList.remove('is-active');
+      showLatLon(pos,lat,lon);
       console.log(pos);
     }).catch((err) => {
-      document.querySelector('.modal .location-message').innerText = geoErrMsg;
+      locMsg.innerText = geoErrMsg;
     });
   }
   document.querySelector('.modal .button').addEventListener('click', () => {
     if (geoCheck()) {
       modal.classList.remove('is-active');
       getPosition().then((pos) => {
+        showLatLon(pos,lat,lon);
         console.log(pos);
       });
     } else {
-      document.querySelector('.modal .location-message').innerText = geoErrMsg;
+      locMsg.innerText = geoErrMsg;
     }
   });
   document.querySelector('.modal-close').addEventListener('click', () => {
@@ -30,6 +35,11 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 });
 
+function showLatLon(pos, latEl, lonEl) {
+  latEl.innerText = `Latitude: ${pos.coords.latitude}`;
+  lonEl.innerText = `Longitude: ${pos.coords.longitude}`;
+}
+// 'https://maps.googleapis.com/maps/api/geocode/json?latlng=-37.7680066,144.9556877&key=AIzaSyBU6f9scZpIBUq0BJwEuyMI25g1Recm7FQ';
 function getPosition () {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject);
