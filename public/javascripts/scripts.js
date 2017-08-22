14b7b4adf44b95037168ce9703e6a177830435d1
Aaron Osteraas <aaron.osteraas@gmail.com>
2017-08-23 08:25:43 +1000

    added wind gust icon to project, moved badge html generation out into functions

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -15,6 +15,7 @@ const icons = {
   cloudCover: `<svg width="45" height="33" viewBox="0 0 45 33" xmlns="http://www.w3.org/2000/svg"><title>Shape</title><path d="M16.945 32.64c-8.835 0-15.998-7.163-15.998-16C.947 7.806 8.11.644 16.945.644c6.004 0 11.23 3.312 13.965 8.203.664-.113 1.338-.205 2.033-.205 6.627 0 12 5.374 12 12s-5.373 12-12 12H16.945zm15.998-4c4.418 0 8-3.583 8-8s-3.582-8-8-8c-1.6 0-3.082.482-4.333 1.292-1.23-5.316-5.974-9.29-11.665-9.29-6.626 0-11.998 5.372-11.998 12 0 6.625 5.372 11.997 11.998 11.997h15.998z" fill-rule="nonzero" fill="#000"/></svg>`,
   rainChance: `<svg width="45" height="45" viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg"><title>Shape</title><path d="M36.943 31.94v-4.38c2.39-1.383 4-3.96 4-6.92 0-4.417-3.582-8-8-8-1.6 0-3.082.48-4.333 1.292-1.23-5.317-5.974-9.29-11.665-9.29-6.626 0-11.998 5.372-11.998 11.998 0 3.55 1.55 6.728 4 8.925v4.916c-4.777-2.767-8-7.92-8-13.84C.947 7.805 8.11.643 16.945.643c6.004 0 11.23 3.31 13.965 8.203.664-.113 1.338-.205 2.033-.205 6.627 0 12 5.373 12 12 0 5.223-3.342 9.653-8 11.3zm-21.997-11.3c1.104 0 2 .897 2 2v16c0 1.104-.896 2-2 2s-2-.896-2-2v-16c0-1.103.896-2 2-2zm8 4c1.103 0 2 .896 2 2v16c0 1.103-.897 2-2 2-1.105 0-2-.897-2-2v-16c0-1.105.895-2 2-2zm7.998-4c1.104 0 2 .897 2 2v16c0 1.104-.896 2-2 2s-2-.896-2-2v-16c0-1.103.896-2 2-2z" fill-rule="nonzero" fill="#000"/></svg>`,
   wind: `<svg width="44" height="28" viewBox="0 0 44 28" xmlns="http://www.w3.org/2000/svg"><title>Shape</title><path d="M38 16h-3c-1.106 0-2-.895-2-2 0-1.103.894-2 2-2h3c1.103 0 2-.895 2-1.998 0-1.105-.897-2-2-2-1.106 0-2-.896-2-2s.894-2 2-2c.136 0 .27.014.4.04 3.122.212 5.597 2.784 5.597 5.96C43.997 13.314 41.312 16 38 16zm-10-4H6c-1.104 0-2 .897-2 2 0 1.105.896 2 2 2H28c3.313 0 6 2.687 6 6 0 3.176-2.476 5.748-5.597 5.96-.13.026-.265.04-.404.04-1.105 0-2-.896-2-2 0-1.105.895-2 2-2 1.104 0 2-.896 2-2s-.896-2-2-2H6c-3.313 0-6-2.686-6-6 0-3.174 2.476-5.746 5.597-5.958.13-.026.265-.04.402-.04H28c1.104 0 2-.896 2-2s-.896-2-2-2c-1.105 0-2-.895-2-2 0-1.104.895-2 2-2 .138 0 .272.015.403.04C31.523.255 34 2.827 34 6.003c0 3.312-2.687 6-6 6z" fill-rule="nonzero" fill="#000"/></svg>`,
+  windGust: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 298.666 298.666"><path d="M162.466 137.052c7.732-7.12 39.268-38.192 36.846-69.387-2.057-26.462-32.63-34.426-49.585-34.426-16.956 0-47.464 7.963-49.52 34.425-2.373 30.564 27.9 61.014 36.41 68.928 3.257-3.25 7.75-5.26 12.716-5.26 5.187 0 9.85 2.207 13.133 5.72zM131.332 149.333c0-3.023.754-5.867 2.07-8.37-11.17-3.414-52.577-14.313-77.824 3-21.89 15.013-13.5 45.47-5.02 60.155 8.478 14.684 30.627 37.122 54.572 25.672 25.487-12.19 37.025-48.342 40.714-62.802-8.27-1.626-14.512-8.908-14.512-17.655zM243.072 144.464c-25.053-17.18-66.036-6.54-77.584-3.028 1.17 2.387 1.844 5.06 1.844 7.897 0 8.808-6.33 16.125-14.686 17.682 3.546 14.076 15.072 50.938 40.874 63.275 23.944 11.452 46.128-11.043 54.606-25.73 8.478-14.682 16.835-45.083-5.054-60.096z"/><path d="M149.333 0C66.99 0 0 66.99 0 149.333s66.99 149.333 149.333 149.333 149.333-66.99 149.333-149.333S231.676 0 149.333 0zm0 282.666C75.813 282.666 16 222.854 16 149.333S75.812 16 149.333 16s133.333 59.813 133.333 133.333-59.813 133.333-133.333 133.333z"/></svg>`,
   sun: `<svg width="48" height="48" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><title>Shape</title><path d="M45.997 26H42c-1.106 0-2-.896-2-2s.894-2 2-2h3.997c1.105 0 2 .896 2 2s-.894 2-2 2zm-7.855-13.312c-.78.78-2.05.78-2.828 0-.78-.78-.78-2.047 0-2.828l2.828-2.828c.78-.78 2.047-.78 2.828 0 .78.78.78 2.047 0 2.828l-2.828 2.828zm-14.14 23.31c-6.628 0-12-5.372-12-11.998 0-6.627 5.37-12 12-12 6.626 0 11.997 5.373 11.997 12 0 6.626-5.372 11.998-12 11.998zM24 16c-4.42 0-8 3.582-8 8 0 4.417 3.582 8 8 8 4.416 0 7.997-3.583 7.997-8 0-4.418-3.58-8-8-8zm0-7.998c-1.106 0-2-.896-2-2v-4c0-1.103.894-2 2-2 1.103 0 2 .897 2 2v4c0 1.104-.898 2-2 2zM9.86 12.688L7.032 9.86c-.78-.78-.78-2.047 0-2.828.78-.78 2.047-.78 2.828 0l2.828 2.828c.78.78.78 2.047 0 2.828-.78.78-2.047.78-2.828 0zM8.002 24c0 1.104-.896 2-2 2h-4c-1.104 0-2-.896-2-2s.897-2 2-2h4c1.105 0 2 .896 2 2zM9.86 35.312c.78-.78 2.047-.78 2.828 0 .78.78.78 2.048 0 2.828L9.86 40.968c-.78.78-2.047.78-2.828 0-.78-.78-.78-2.047 0-2.828l2.828-2.828zM24 39.998c1.105 0 2 .895 2 2v4c0 1.103-.895 2-2 2-1.104 0-2-.897-2-2v-4c0-1.105.896-2 2-2zm14.142-4.686l2.828 2.828c.78.78.78 2.048 0 2.828-.78.78-2.05.78-2.828 0l-2.828-2.828c-.78-.78-.78-2.047 0-2.828.78-.78 2.046-.78 2.828 0z" fill-rule="nonzero" fill="#000"/></svg>`,
   compass: `<svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg"><title>Shape</title><path d="M14 .002C6.27.002 0 6.27 0 14c0 7.73 6.27 14 14 14s13.998-6.27 13.998-14S21.73.002 14 .002zM14 24C8.478 24 4 19.52 4 14 4 8.48 8.48 4.002 14 4.002c5.52 0 10 4.477 10 9.998 0 5.522-4.48 10-10 10zm-4-10c0 2.21 1.79 4 4 4s4-1.79 4-4-4-8-4-8-4 5.79-4 8zm5 0c0 .553-.448 1-1 1-.553 0-1-.447-1-1 0-.552.447-1 1-1 .552 0 1 .448 1 1z" fill-rule="nonzero" fill="#000"/></svg>`,
   pressure: `<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><title>Shape</title><path d="M13.014 11.002c-1.107.008-2.008-.884-2.016-1.987-.01-1.107.88-2.007 1.987-2.016.166-.002.324.022.478.06 1.156-.906 2.667-2.086 2.84-2.21.297-.21.64-.235.882.008.24.248.215.623-.01.886-.077.09-1.295 1.622-2.23 2.798.035.143.057.29.058.444.01 1.102-.88 2.008-1.99 2.016zM0 21c0-.553.448-1 1-1h9v-2.525C6.51 16.235 4 12.91 4 9c0-4.962 4.038-9 9-9 4.963 0 9 4.038 9 9 0 3.91-2.51 7.236-6 8.475V20h9c.553 0 1 .447 1 1 0 .553-.447 1-1 1H1c-.552 0-1-.447-1-1zm13-6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6zm12 9H1c-.552 0-1 .447-1 1 0 .553.448 1 1 1h24c.553 0 1-.447 1-1 0-.553-.447-1-1-1z" fill-rule="nonzero" fill="#030104"/></svg>`,
@@ -109,50 +110,13 @@ function displayCurrent(currently, todaysForecast, location, units) {
           </div>
           <div class="columns">
             <div class="column">
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.thermometerQuarter}</div>
-                <div class="info-time">${todaysForecast.temperatureMin} ${units.temp}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.thermometerFull}</div>
-                <div class="info-time">${todaysForecast.temperatureMax} ${units.temp}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.sunrise}</div>
-                <div class="info-time">${timeGenerator(todaysForecast.sunriseTime)}</div>
-               </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.sunset}</div>
-                <div class="info-time">${timeGenerator(todaysForecast.sunsetTime)}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.sun}</div>
-                <div class="info-time">${currently.uvIndex}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.cloudCover}</div>
-                <div class="info-time">${asPercentageText(todaysForecast.cloudCover)}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.rainChance}</div>
-                <div class="info-time">${asPercentageText(todaysForecast.precipProbability)}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.wind}</div>
-                <div class="info-time">${todaysForecast.windSpeed} ${units.windSpeed}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon" style="transform:rotate(${currently.windBearing}deg)">${icons.compass}</div>
-                <div class="info-time">${degToCompass(todaysForecast.windBearing)}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.pressure}</div>
-                <div class="info-time">${todaysForecast.pressure.toFixed()} ${units.pressure}</div>
-              </div>
-              <div class="info-item has-text-centered">
-                <div class="info-icon">${icons.humidity}</div>
-                <div class="info-time">${asPercentageText(todaysForecast.humidity)}</div>
-              </div>
+              ${temperatureBadges(todaysForecast, units)}
+              ${sunriseSunsetBadges(todaysForecast)}
+              ${uvIndexBadge(currently)}
+              ${cloudCoverRainChanceBadges(currently)}
+              ${windSpeedGustBearingBadges(todaysForecast, units)}
+              ${pressureBadge(todaysForecast, units)}
+              ${humidityBadge(currently)}
             </div>
           </div>
           <div class="columns">
@@ -169,22 +133,11 @@ function displayCurrent(currently, todaysForecast, location, units) {
       </div>`;
 }
 
-function timeGenerator(date) {
-  let d = new Date(date * 1000);
-  let h = hourMinFormatter(d.getHours());
-  let m = hourMinFormatter(d.getMinutes());
-  return `${h}:${m}`;
-}
-
-function dayGenerator(date) {
-  return days[new Date(date * 1000).getDay()];
-}
-
 function displayForecast(forecast, units) {
   console.log(forecast);
 
   let forecastHtml = [
-      `<div class="columns">
+    `<div class="columns">
         <div class="column">
           <hr>
           <h5 class="title is-5 has-text-centered">${forecast.length} Day Forecast</h5>
@@ -209,50 +162,13 @@ function displayForecast(forecast, units) {
                        </div>
                        <div class="columns">
                          <div class="column">
-                           <div class="info-item has-text-centered">
-                             <div class="info-icon">${icons.thermometerQuarter}</div>
-                             <div class="info-time">${f.temperatureMin} ${units.temp}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                             <div class="info-icon">${icons.thermometerFull}</div>
-                             <div class="info-time">${f.temperatureMax} ${units.temp}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                              <div class="info-icon">${icons.sunrise}</div>
-                              <div class="info-time">${timeGenerator(f.sunriseTime)}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                              <div class="info-icon">${icons.sunset}</div>
-                              <div class="info-time">${timeGenerator(f.sunsetTime)}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                             <div class="info-icon">${icons.sun}</div>
-                             <div class="info-time">${f.uvIndex}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                              <div class="info-icon">${icons.cloudCover}</div>
-                              <div class="info-time">${asPercentageText(f.cloudCover)}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                              <div class="info-icon">${icons.rainChance}</div>
-                              <div class="info-time">${asPercentageText(f.precipProbability)}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                              <div class="info-icon">${icons.wind}</div>
-                              <div class="info-time">${f.windSpeed}${units.windSpeed}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                              <div class="info-icon" style="transform:rotate(${f.windBearing}deg)">${icons.compass}</div>
-                              <div class="info-time">${degToCompass(f.windBearing)}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                             <div class="info-icon">${icons.pressure}</div>
-                             <div class="info-time">${f.pressure} ${units.pressure}</div>
-                           </div>
-                           <div class="info-item has-text-centered">
-                             <div class="info-icon">${icons.humidity}</div>
-                             <div class="info-time">${asPercentageText(f.humidity)}</div>
-                           </div>
+                           ${temperatureBadges(f, units)}
+                           ${sunriseSunsetBadges(f)}
+                           ${uvIndexBadge(f)}
+                           ${cloudCoverRainChanceBadges(f)}
+                           ${windSpeedGustBearingBadges(f, units)}
+                           ${pressureBadge(f, units)}
+                           ${humidityBadge(f)}
                          </div>
                          </div>
                        </div>
@@ -263,6 +179,91 @@ function displayForecast(forecast, units) {
   });
   dqs('.forecast').innerHTML = forecastHtml.join('');
 }
+
+function timeGenerator(date) {
+  let d = new Date(date * 1000);
+  let h = hourMinFormatter(d.getHours());
+  let m = hourMinFormatter(d.getMinutes());
+  return `${h}:${m}`;
+}
+function temperatureBadges(w, u) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.thermometerQuarter}</div>
+            <div class="info-time">${w.temperatureMin} ${u.temp}</div>
+          </div>
+          <div class="info-item has-text-centered">
+            <div class="info-icon">${icons.thermometerFull}</div>
+            <div class="info-time">${w.temperatureMax} ${u.temp}</div>
+          </div>`;
+}
+function humidityBadge(w) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.humidity}</div>
+            <div class="info-time">${asPercentageText(w.humidity)}</div>
+          </div>`;
+}
+function cloudCoverRainChanceBadges(w) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.cloudCover}</div>
+            <div class="info-time">${asPercentageText(w.cloudCover)}</div>
+          </div>
+          <div class="info-item has-text-centered">
+            <div class="info-icon">${icons.rainChance}</div>
+            <div class="info-time">${asPercentageText(w.precipProbability)}</div>
+          </div>`;
+}
+
+function sunriseSunsetBadges(w) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.sunrise}</div>
+            <div class="info-time">${timeGenerator(w.sunriseTime)}</div>
+          </div>
+          <div class="info-item has-text-centered">
+            <div class="info-icon">${icons.sunset}</div>
+            <div class="info-time">${timeGenerator(w.sunsetTime)}</div>
+          </div>`;
+}
+
+function uvIndexBadge(w) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.sun}</div>
+            <div class="info-time">${w.uvIndex}</div>
+          </div>`
+}
+
+function windSpeedGustBearingBadges(w, u) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.wind}</div>
+            <div class="info-time">${w.windSpeed} ${u.windSpeed}</div>
+          </div>
+          <div class="info-item has-text-centered">
+            <div class="info-icon">${icons.windGust}</div>
+            <div class="info-time">${w.windGust} ${u.windSpeed}</div>
+          </div>
+          <div class="info-item has-text-centered">
+            <div class="info-icon" style="transform:rotate(${w.windBearing}deg)">${icons.compass}</div>
+            <div class="info-time">${degToCompass(w.windBearing)}</div>
+          </div>`
+}
+function pressureBadge(w, u) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.pressure}</div>
+            <div class="info-time">${w.pressure.toFixed()} ${u.pressure}</div>
+          </div>`;
+}
+
+function humidityBadge(w) {
+  return `<div class="info-item has-text-centered">
+            <div class="info-icon">${icons.humidity}</div>
+            <div class="info-time">${asPercentageText(w.humidity)}</div>
+          </div>`;
+}
+
+function dayGenerator(date) {
+  return days[new Date(date * 1000).getDay()];
+}
+
+
 
 function dateGenerator(time) {
   let d = new Date(time * 1000);
