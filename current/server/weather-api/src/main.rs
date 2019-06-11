9a3ef28fd5cfb35bca34c5afe16635ffdb5ee1a1
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-11 12:16:47 +1000

    start on endpoints

---
+++ current/server/weather-api/src/main.rs
@@ -0,0 +1,34 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::json::JsonValue;
+use rocket_contrib::json::Json;
+mod location;
+use location::{Location};
+
+#[post("/", format="json", data ="<location>")]
+fn location(location: Json<Location>) -> Json<Location> {
+    let loc = Location{
+        lat : location.lat,
+        lon : location.lon,
+    };
+    Json(loc)
+}
+
+#[get("/")]
+fn get_json() -> JsonValue {
+    json!({
+        "key": "value",
+        "array": [1, 2, 3, 4]
+    })
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/api/json", routes![get_json])
+        .mount("/api/location", routes![location])
+        .launch();
+}
