d7080592703f9ad88f1627ed1273d0e1921fb18b
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-24 19:57:13 +1000

    Add some more components and experiment with layout

--- current/client/src/components/App.tsx
+++ current/client/src/components/App.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { WiDaySunny } from 'weather-icons-react';
 
 const AppWrapper = styled.div`
   height: 100vh;
   background-color: ${props => props.theme.black};
 `;
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const Button = styled.button`
+  color: ${p => p.theme.black};
+  background-color: ${p => p.theme.white};
+  padding: 1rem;
+`;
+const lat = -37.7692497;
+const lon = 144.9506981;
+const location = { lat, lon };
 
 const App: React.FC = () => {
+  const getWeather = async () => {
+    //
+  };
+
   return (
     <AppWrapper>
       <header className="App-header">
-        <p>Weather Goes Here</p>
+        <WiDaySunny size={24} color="#FFF" />{' '}
       </header>
+      <Main>
+        <div>
+          <Button onClick={() => getWeather()}>Make things happen</Button>
+        </div>
+      </Main>
     </AppWrapper>
   );
 };
