58e22d21e6f13b34adc9d1542f0b5a71373d5eed
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-24 20:30:51 +1000

    fix formatting

--- current/server/lib/weather/weather_router.ex
+++ current/server/lib/weather/weather_router.ex
@@ -1,23 +1,27 @@
 defmodule Weather.Router do
-    use Plug.Router
-    use Plug.Debugger
-    require Logger
-plug(Plug.Logger, log: :debug)
-plug(:match)
-plug(:dispatch)
-# routes
-    get "/hello" do
-        send_resp(conn, 200, "World")
-    end
+  use Plug.Router
+  use Plug.Debugger
+  require Logger
+  plug(Plug.Logger, log: :debug)
+  plug(:match)
+  plug(:dispatch)
+  # routes
+  get "/hello" do
+    send_resp(conn, 200, "World")
+  end
 
-    post "/post" do
-       {:ok, body, conn} = read_body(conn)
-       body = Poison.decode!(body)
-       IO.inspect(body)
-       send_resp(conn, 201, "created: #{get_in(body, ["message"])}")
-    end
+  post "/post" do
+    {:ok, body, conn} = read_body(conn)
+    body = Poison.decode!(body)
 
-    match _ do
-        send_resp(conn, 404, "not found")
-    end
+    latitude = body["lat"]
+    longitude = body["lat"]
+
+    IO.inspect(body)
+    send_resp(conn, 201, "created: #{get_in(body, ["message"])}")
+  end
+
+  match _ do
+    send_resp(conn, 404, "not found")
+  end
 end
