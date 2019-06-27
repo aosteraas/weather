0cdc4664d00acfb872e295d46ede4c46cbdf7698
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-27 14:39:37 +1000

    return more meaningful data from weather api request
    add private function for generating the request url reading in app env vars

--- current/server/lib/weather/weather_darksky.ex
+++ current/server/lib/weather/weather_darksky.ex
@@ -1,20 +1,24 @@
 defmodule Weather.Darksky do
   use HTTPoison.Base
+  @api_base "https://api.darksky.net/forecast"
 
-  def getWeather(latitude, longitude) do
-    #
-    url = "https://api.darksky.net/forecast/#{key}/#{latitude},#{longitude}?exclude=[minutely]"
+  def get_weather(coordinates) do
+    url = create_url(coordinates)
 
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        IO.puts(body)
-        body
+        %{body: body, status_code: 200}
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts("Not Found :(")
+        %{body: "Not Found", status_code: 404}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect(reason)
+        %{body: reason, status_code: 500}
     end
+  end
+
+  defp create_url(coordinates) do
+    key = Application.fetch_env!(:weather, :api_key)
+    "#{@api_base}/#{key}/#{coordinates.lat},#{coordinates.lon}?exclude=[minutely,flags]"
   end
 end
