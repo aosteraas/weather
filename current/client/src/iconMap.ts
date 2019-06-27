11f5f556d939432cb0a84d9fd5a51359b7e19487
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-27 18:14:57 +1000

    create icon map from dark sky to react weather icons

---
+++ current/client/src/iconMap.ts
@@ -0,0 +1,12 @@
+export const iconMap = {
+  'clear-day': 'WiDaySunny',
+  'clear-night': 'WiNightClear',
+  rain: 'WiRainMix',
+  snow: 'WiSnow',
+  sleet: 'WiSleet',
+  wind: 'WiStrongWind',
+  fog: 'WiFog',
+  cloudy: 'WiCloud',
+  'partly-cloudy-day': 'WiDayCloudy',
+  'partly-cloudy-night': 'WiNightAltPartlyCloudy'
+};
