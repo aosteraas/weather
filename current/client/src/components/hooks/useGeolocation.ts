37553175917537d3551efb486176631ccd8245d2
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-27 15:27:26 +1000

    import useState to store geolocation, set correct types, fix typo

--- current/client/src/components/hooks/useGeolocation.ts
+++ current/client/src/components/hooks/useGeolocation.ts
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
 interface UseGeolocation {
   getLocation(): void;
 }
-interface Postion {
+interface Position {
   coords: Coordinates;
+  timestamp: number;
 }
 interface Coordinates {
   latitude: number;
   longitude: number;
 }
 export function useGeolocation(): UseGeolocation {
-  const getLocation = () => {};
+  const [location, setLocation] = useState();
+
+  const onSuccess: PositionCallback = (pos: Position) => {
+    const { latitude, longitude } = pos.coords;
+    setLocation({ latitude, longitude });
+    console.log(latitude, longitude);
+  };
+
+  const onError: PositionErrorCallback = (positionError: PositionError) => {};
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  };
   return { getLocation };
 }
