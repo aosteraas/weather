13e03af6bcd25b11602b5eadf1e04a1e3cc4df86
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-27 15:56:28 +1000

    use local storage hook in geolocation hook

--- current/client/src/components/hooks/useGeolocation.ts
+++ current/client/src/components/hooks/useGeolocation.ts
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocalStorage } from './';
 
 interface UseGeolocation {
   getLocation(): void;
@@ -7,12 +8,23 @@ interface Position {
   coords: Coordinates;
   timestamp: number;
 }
-interface Coordinates {
+
+export interface Coordinates {
   latitude: number;
   longitude: number;
 }
 export function useGeolocation(): UseGeolocation {
   const [location, setLocation] = useState();
+  const { saveLocation, restoreLocation } = useLocalStorage();
+
+  const savedLocation = restoreLocation();
+
+  useEffect(() => {
+    if (savedLocation) {
+      setLocation(savedLocation);
+      // getWeather
+    }
+  }, [savedLocation]);
 
   const onSuccess: PositionCallback = (pos: Position) => {
     const { latitude, longitude } = pos.coords;
@@ -25,5 +37,6 @@ export function useGeolocation(): UseGeolocation {
   const getLocation = () => {
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
   };
+
   return { getLocation };
 }
