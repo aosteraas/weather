9ff31100c4474f6580f5857e908d952cce3788c3
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-27 18:37:01 +1000

    Change icon map structure for great succcess very nice

--- current/client/src/iconMap.ts
+++ current/client/src/iconMap.ts
@@ -1,12 +1,25 @@
-export const iconMap = {
-  'clear-day': 'WiDaySunny',
-  'clear-night': 'WiNightClear',
-  rain: 'WiRainMix',
-  snow: 'WiSnow',
-  sleet: 'WiSleet',
-  wind: 'WiStrongWind',
-  fog: 'WiFog',
-  cloudy: 'WiCloud',
-  'partly-cloudy-day': 'WiDayCloudy',
-  'partly-cloudy-night': 'WiNightAltPartlyCloudy'
-};
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiRainMix,
+  WiSnow,
+  WiSleet,
+  WiStrongWind,
+  WiFog,
+  WiCloud,
+  WiDayCloudy,
+  WiNightAltPartlyCloudy
+} from 'weather-icons-react';
+
+export const iconMap = [
+  { icon: 'clear-day', Component: WiDaySunny },
+  { icon: 'clear-night', Component: WiNightClear },
+  { icon: 'rain', Component: WiRainMix },
+  { icon: 'snow', Component: WiSnow },
+  { icon: 'sleet', Component: WiSleet },
+  { icon: 'wind', Component: WiStrongWind },
+  { icon: 'fog', Component: WiFog },
+  { icon: 'cloudy', Component: WiCloud },
+  { icon: 'partly-cloudy-day', Component: WiDayCloudy },
+  { icon: 'partly-cloudy-night', Component: WiNightAltPartlyCloudy }
+];
