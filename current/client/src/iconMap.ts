594120c3c62f0c9e5cab8e30bce1fb37e2cba33c
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-27 22:24:18 +1000

    add some more weather icons and getter fn

--- current/client/src/iconMap.ts
+++ current/client/src/iconMap.ts
@@ -8,7 +8,11 @@ import {
   WiFog,
   WiCloud,
   WiDayCloudy,
-  WiNightAltPartlyCloudy
+  WiNightAltPartlyCloudy,
+  WiHumidity,
+  WiThermometer,
+  WiWindy,
+  WiCloudy
 } from 'weather-icons-react';
 
 export const iconMap = [
@@ -17,9 +21,21 @@ export const iconMap = [
   { icon: 'rain', Component: WiRainMix },
   { icon: 'snow', Component: WiSnow },
   { icon: 'sleet', Component: WiSleet },
-  { icon: 'wind', Component: WiStrongWind },
+  { icon: 'wind', Component: WiWindy },
   { icon: 'fog', Component: WiFog },
   { icon: 'cloudy', Component: WiCloud },
   { icon: 'partly-cloudy-day', Component: WiDayCloudy },
-  { icon: 'partly-cloudy-night', Component: WiNightAltPartlyCloudy }
+  { icon: 'partly-cloudy-night', Component: WiNightAltPartlyCloudy },
+  { icon: 'humidity', Component: WiHumidity },
+  { icon: 'thermometer', Component: WiThermometer },
+  { icon: 'windspeed', Component: WiStrongWind },
+  { icon: 'cloudcover', Component: WiCloudy }
 ];
+
+export const getIcon = (icon: string) => {
+  const iconable = iconMap.find(x => x.icon === icon);
+  if (iconable) {
+    return iconable.Component;
+  }
+  return WiCloud;
+};
