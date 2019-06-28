f1ea102c7e98a75fbad58a78746657b3d898de79
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-06-28 22:44:58 +1000

    add header border, main padding, remove main jsutify content

--- current/client/src/components/styles/AppStyle.ts
+++ current/client/src/components/styles/AppStyle.ts
@@ -8,12 +8,13 @@ export const AppStyle = styled.div`
     justify-content: center;
     align-items: center;
     padding: 1.5rem;
+    border-bottom: 1px solid ${p => p.theme.white};
   }
 `;
 
 export const Main = styled.main`
+  padding: 2rem;
   display: flex;
-  justify-content: center;
   align-items: center;
   height: 100%;
 `;
