6326614fcb02c560945513d3a62b6efc4e918468
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-07-03 13:26:47 +1000

    update and add base props to theme

--- current/client/src/components/styles/theme.ts
+++ current/client/src/components/styles/theme.ts
@@ -1,5 +1,15 @@
 export const theme = {
-  black: `#282c34`,
-  blue: `#61dafb`,
-  white: `#ffffff`
+  breakpoints: ['40em', '52em', '64em'],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  colors: {
+    black: `#282c34`,
+    white: `#ffffff`,
+    blue: `#2e7efc`,
+    lightBlue: `#5faafc`
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256],
+  shadows: {
+    small: '0 0 4px rgba(0, 0, 0, .125)',
+    large: '0 0 24px rgba(0, 0, 0, .125)'
+  }
 };
