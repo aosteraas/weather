db7e19a8282bab082dd8ab05ad34a8bc0fdf26b1
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-07-18 11:37:17 +1000

    export typed object of formattters

--- current/client/src/lib/formatter.ts
+++ current/client/src/lib/formatter.ts
@@ -1,10 +1,31 @@
-export const formatTime = (time: number): string => {
-  return new Date(time * 1000).toLocaleTimeString();
+interface IFormatter {
+  time(value: number): string;
+  percent(value: number): string;
+  temp(value: number): string;
+  wind(value: number): string;
+}
+export const format: IFormatter = {
+  time,
+  temp,
+  percent,
+  wind
 };
 
-export const formatPercent = (value: number): string => {
+function time(time: number): string {
+  return new Date(time * 1000).toLocaleTimeString();
+}
+
+function temp(value: number): string {
+  return `${value.toFixed(1)}º`;
+}
+
+function percent(value: number): string {
   return `${(value * 100).toFixed(0)}%`;
-};
+}
+
+function wind(value: number): string {
+  return `${value.toFixed(0)}`;
+}
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
