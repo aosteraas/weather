b74a9c10efa6380fd6b93f40e9b97bf7d01de171
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-07-18 10:37:07 +1000

    Switch to map array of desired keys

--- current/client/src/components/Overview.tsx
+++ current/client/src/components/Overview.tsx
@@ -2,23 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import { Flex, Box } from 'rebass';
 import { DailyData } from '../types';
+
 const Item = styled(Box)`
   border-bottom: 1px solid ${p => p.theme.colors.darkGrey};
   &:nth-of-type(odd) {
     border-right: 1px solid ${p => p.theme.colors.darkGrey};
   }
 `;
+
 export const Overview: React.FC<Props> = ({ overview }) => {
+  const keys = [
+    'temperatureHigh',
+    'temperatureLow',
+    'precipProbability',
+    'windSpeed',
+    'windGust',
+    'humidity',
+    'pressure'
+  ];
   return (
     <Flex backgroundColor="white" color="black" flexWrap="wrap">
-      <Item width={1 / 2}>{overview.temperatureHigh}</Item>
-      <Item width={1 / 2}>{overview.temperatureLow}</Item>
+      {keys.map((k, idx) => (
+        <Item key={idx} width={1 / 2}>
+          {overview[k]}
+        </Item>
+      ))}
       <Item width={1 / 2}>rain so far ?</Item>
-      <Item width={1 / 2}>{overview.precipProbability}</Item>
-      <Item width={1 / 2}>{overview.windSpeed}</Item>
-      <Item width={1 / 2}>{overview.windGust}</Item>
-      <Item width={1 / 2}>{overview.humidity}</Item>
-      <Item width={1 / 2}>{overview.pressure}</Item>
     </Flex>
   );
 };
