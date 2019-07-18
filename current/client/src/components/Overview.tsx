a702e981baa0bd3d15c9fcf7af6e348fcfdc6605
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-07-18 11:08:22 +1000

    swtich keys to typed object

--- current/client/src/components/Overview.tsx
+++ current/client/src/components/Overview.tsx
@@ -1,32 +1,76 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Flex, Box } from 'rebass';
+import { Flex, Box, Text } from 'rebass';
+import { getIcon } from '../lib/iconMap';
 import { DailyData } from '../types';
 
 const Item = styled(Box)`
+  display: flex;
+  align-items: center;
+  height: 5.6rem;
   border-bottom: 1px solid ${p => p.theme.colors.darkGrey};
   &:nth-of-type(odd) {
     border-right: 1px solid ${p => p.theme.colors.darkGrey};
   }
 `;
 
+const IconBox = styled.div<IconBoxProps>`
+  background-color: ${p => p.theme.icon[p.iconColor]};
+  color: ${p => p.theme.colors.white};
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Overview: React.FC<Props> = ({ overview }) => {
-  const keys = [
-    'temperatureHigh',
-    'temperatureLow',
-    'precipProbability',
-    'windSpeed',
-    'windGust',
-    'humidity',
-    'pressure'
-  ];
+  const keys: DataKeys = {
+    temperatureHigh: {
+      icon: 'thermometer',
+      iconColor: 'red'
+    },
+    temperatureLow: {
+      icon: 'thermometer',
+      iconColor: 'blue'
+    },
+    precipProbability: {
+      icon: 'rain',
+      iconColor: 'blue'
+    },
+    windSpeed: {
+      icon: 'wind',
+      iconColor: 'green'
+    },
+    windGust: {
+      icon: 'windGust',
+      iconColor: 'green'
+    },
+    humidity: {
+      icon: 'humidity',
+      iconColor: 'blue'
+    },
+    pressure: {
+      icon: 'pressure',
+      iconColor: 'blue'
+    }
+  };
   return (
     <Flex backgroundColor="white" color="black" flexWrap="wrap">
-      {keys.map((k, idx) => (
-        <Item key={idx} width={1 / 2}>
-          {overview[k]}
-        </Item>
-      ))}
+      {Object.keys(keys).map((k, idx) => {
+        const { icon, iconColor } = keys[k];
+        const Icon = getIcon(icon);
+
+        return (
+          <Item key={idx} width={1 / 2}>
+            <IconBox iconColor={iconColor}>
+              <Icon />
+            </IconBox>
+            <Text>{overview[k]}</Text>
+          </Item>
+        );
+      })}
       <Item width={1 / 2}>rain so far ?</Item>
     </Flex>
   );
@@ -34,4 +78,16 @@ export const Overview: React.FC<Props> = ({ overview }) => {
 
 interface Props {
   overview: DailyData;
+}
+
+interface IconBoxProps {
+  iconColor: string;
+}
+interface DataValue {
+  icon: string;
+  iconColor: string;
+}
+
+interface DataKeys {
+  [key: string]: DataValue;
 }
