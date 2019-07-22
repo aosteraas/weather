cac1e0cd3865e8633941e711e8773c61495dcc01
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-07-22 17:36:35 +1000

    Move spacer component into separate file

---
+++ current/rnw-client/src/screens/App/Spacer.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+const Spacer = styled.View`
+  height: 16px;
+  background-color: #f8f8f8;
+  border-bottom-color: #e4e4e4;
+  border-bottom-width: 1;
+  border-top-color: #e4e4e4;
+  border-top-width: 1;
+`;
+
+export default Spacer;
